e9c70a8a2360221f0a26015770cbfc53d5608494
Ashraf Abu Bakr <ashraf.abubakr@itworxedu.com>
2022-03-09 15:06:43 +0200

    - Semantic direction manager.

---
+++ Sources/CalendarSemanticDirectionManager.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarSemanticDirectionManager.swift
+//  CalendarKit
+//
+//  Created by Ashraf Abubakr on 09/03/2022.
+//
+
+import Foundation
+
+public final class CalendarSemanticDirectionManager {
+    public static let shared: CalendarSemanticDirectionManager = CalendarSemanticDirectionManager()
+    public var calendarLayoutDirection: UIUserInterfaceLayoutDirection = .leftToRight
+}
